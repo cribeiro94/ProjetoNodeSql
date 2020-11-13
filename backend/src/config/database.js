f522450b85b7d22a381aa56a09e37e8c91780684
Caio Vinicius Ribeiro de Mello <caio.mello@evalsaude.com.br>
2020-11-13 12:10:43 -0300

    Atualização de meu backend

---
+++ backend/src/config/database.js
@@ -0,0 +1,11 @@
+   module.exports = {
+    dialect: 'postgres',
+    server: 'localhost',
+    username: 'caioadm',
+    password: '12345678',
+    database: 'dbcrypto',
+    define: {
+       timestamps: true,
+       underscored: true,
+    },
+};
