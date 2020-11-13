9da4603a021e0cfcea51ed8445c09e338f567511
Caio Vinicius Ribeiro de Mello <caio.mello@evalsaude.com.br>
2020-11-13 12:08:09 -0300

    Atualizando meu backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,24 @@
+const cors = require('cors');
 const express = require('express');
 const routes = require('./routes');
 
+
 require('./database');
+
 
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    //console.log("Acessou o Middleware!");
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", 'X-PINGOTHER,Content-Type');
+    app.use(cors());
+    next();
+})
+
 app.use(routes);
 
 app.listen(3333);
